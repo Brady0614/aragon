014800f131143986f81d72356bfbc44336b85f81
Gorka <gorka@aquigorka.com>
2019-02-15 11:12:22 +0100

    MenuButton: refactor to use button icon and icon menu

--- src/components/MenuPanel/MenuButton.js
+++ src/components/MenuPanel/MenuButton.js
@@ -1,31 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
-import { theme } from '@aragon/ui'
-import IconMenu from '../../icons/IconMenu'
-
-const StyledButton = styled.button`
-  border: none;
-  background: none;
-  margin-left: 24px;
-  height: 32px;
-  width: 32px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0;
-  cursor: pointer;
-  outline: none;
-
-  &:focus {
-    border: 2px solid ${theme.accent};
-  }
-  &:active {
-    border: none;
-  }
-`
+import { ButtonIcon, IconMenu } from '@aragon/ui'
 
 export default props => (
-  <StyledButton {...props}>
+  <ButtonIcon {...props}>
     <IconMenu />
-  </StyledButton>
+  </ButtonIcon>
 )
